19158e0ac81bde267680e14e869648c60452421a
Is44cNascimento <isaacnascimento.jesus@ucsal.edu.br>
2025-10-27 10:51:13 -0300

    Mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,27 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args) {
+
+
+        Mentoria mentoriaA = new Mentoria();
+        Curso cursoA = new Curso();
+
+
+        cursoA.setTitulo("Estrutura de dados");
+        cursoA.setDescricao("Aula de estrura de dados. A base de todo sistema");
+        cursoA.setCargaHoraria(160);
+
+
+
+        mentoriaA.setTitulo("Recursividade");
+        mentoriaA.setDescricao("Mentoria de como utilizar recursividade e como funciona");
+        mentoriaA.setData(LocalDate.now());
+
+        mentoriaA.ToString();
+        cursoA.ToString();
+    }
 }
