038f4ad37956bd6794957251dafcb8aee16259fd
Is44cNascimento <isaacnascimento.jesus@ucsal.edu.br>
2025-11-02 09:41:33 -0300

    Dev e Bootcamp

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -1,9 +1,6 @@
 package br.com.dio.desafio.dominio;
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 public class Dev {
 
@@ -14,12 +11,19 @@ public class Dev {
 
 
     public void inscreverBootCamp(Bootcamp bootcamp) {
-
+        this.conteudoInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
     }
 
 
     public void progredir(){
-
+        Optional<Conteudo> conteudo =  this.conteudoConcluidos.stream().findFirst();
+        if(conteudo.isPresent()){
+            this.conteudoConcluidos.add(conteudo.get());
+            this.conteudoConcluidos.remove(conteudo.get());
+        }else {
+            IO.println("Nenhum conteudo foi encontrado!");
+        }
     }
 
     public void calcularXp(){
